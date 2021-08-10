a524f7270b6acab93a95bc0f6448636c63513059
Ian <iantskon@gmail.com>
2021-08-09 23:08:31 -0500

    Add hourly_rate from https://api.thenewboston.com/core_members

--- app.js
+++ app.js
@@ -20,17 +20,28 @@ const axios = require('axios');
 
 (async()=> {
     try {
+        // get core-team data
+        const response = await axios.get('https://api.thenewboston.com/core_members');
+        const teamMembers = response.data.results;
+        // initialize empty array for bank transactions
         const txs = [];
         // const { data } = await octokit.request('GET /issues');
         const { data } = await axios.get('https://api.github.com/repos/thenewboston-developers/Contributor-Payments/issues?state=open&labels=Timesheet,%F0%9F%93%9D%20Ready%20for%20Review%F0%9F%93%9D');
         for(const issue of data) {
-            const { body, number, title } = issue;
+            const { body, number, title, user } = issue;
             // parse recipient account number
             const recipient = body.match(/[a-z0-9]{64}/) ? body.match(/[a-z0-9]{64}/)[0] : false;
             let totalTimeSpent = body.match(/total time spent\r\n\d+\.*\.?5? hour/i) ? body.match(/total time spent\r\n\d+\.*\.?5? hour/i)[0] : false;
             totalTimeSpent = totalTimeSpent ? totalTimeSpent.match(/\d+\.*\.?5?/)[0] : false;
-            // need users to include their rate or need to pull from another data source
-            const rate = 800;
+            // pull username and match from teamMembers to pull hourly rate
+            const member = teamMembers.find(member => {
+                if (member.user.github_username === user.login ||
+                title.toLowerCase().includes(member.user.display_name.toLowerCase()) ||
+                member.user.account_number === recipient) {
+                    return member;
+                }
+            });
+            const rate = member && member.hourly_rate ? member.hourly_rate : 0;
             // calculate amount to be paid
             const amount = rate * totalTimeSpent;
             const issueId = number;
@@ -38,11 +49,19 @@ const axios = require('axios');
             date = date ? date[0].replace(/\//g, '_') : false;
             // generate memo
             const memo = `TNB_TS_${issueId}_${date}`;
-            txs.push({
-                amount,
-                memo,
-                recipient,
-            });
+            if (amount && memo && recipient) {
+                txs.push({
+                    amount,
+                    memo,
+                    recipient,
+                });
+            } else {
+                console.log('Missing data for:', title, {
+                    amount,
+                    memo,
+                    recipient,
+                });
+            }
         }
         console.log('Transaction Data for Bulk Payment:', txs);
         // send bulk payments to the blockchain
