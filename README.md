2b9bf14cd59257f5151a31656897e6d3045bafd7
Ian <iantskon@gmail.com>
2021-08-16 11:53:45 -0500

    Add error handling, log coloring, and readme instructions

--- README.md
+++ README.md
@@ -1,9 +1,23 @@
 # The New Boston - Core Team Automated Payroll Script
 
+## To run:
+- clone repo and install dependencies
+    - `git clone git@github.com:nax3t/automated-payroll.git`
+    - `cd automated-payroll`
+    - `npm install`
+- Rename example.env to .env and populate with required values
+- Change testing variable to false in app.js (if not already false)
+- Run the program with `node app.js`
+
+## Error key:
+- green: payment succeeded
+- red: payment failed (see error)
+- yellow: payment held for manual review due to amount over 10,000 TNBC
+
 ### Script Notes:
 - The script will automatically pull up all the issues from thenewboston-github repositories, where payments need to do, with the Ready to pay label added to them and in the open state.
     - [X] Check that 'Ready to pay' label added by correct user
-    - [ ] Update id being used for 'Ready to pay' label user check
+    - [x] Update ids being used for 'Ready to pay' label user check
     - [X] Need to update the labels being used in the code to pull in Ready to Pay Timesheet issues from the API
 - Collect this info from issues and core members API:
     - [x] Contributor account address
@@ -13,14 +27,14 @@
     - [x] Issue id
 - [x] Print any transactions where insufficient data is available to make a valid transaction, e.g., no hourly rate
 - [x] Generate appropriate memo for each payment based on standard memo format
-- [ ] Print payment sheet. Highlight those issues with more than 10000TNBC to be sent at once.
+- [x] Print payment sheet. Highlight those issues with more than 10000TNBC to be sent at once.
 - [ ] Payer will run the pay command after reviewing the payment sheet.
     - [ ] Use either a mailer notification or create a new github issue for these kinds of manual verification payments
-- [ ] Payment by the script:
+- [x] Payment by the script:
 - [x] Send coins to the contributor
-- [ ] Confirm transaction is successful
+- [x] Confirm transaction is successful
 - [x] Add Paid label to the issue
 - [x] Remove Ready to pay label to the issue
 - [x] Close the issue
-- [ ] Print all successful and failed payments
+- [x] Print all successful and failed payments
 - [ ] Use agenda js to create a background job that runs the script daily on a production server
--- processSingleIssue.js
+++ processSingleIssue.js
@@ -1,36 +1,47 @@
+const fs = require('fs');
+const util = require('util');
+const writeFile = util.promisify(fs.writeFile);
+const appendFile = util.promisify(fs.appendFile);
 const axios = require('axios');
-const issueId = 'PASTE_ISSUE_ID_HERE';
+const { time } = require('console');
+const issueIds = process.argv.slice(2);
 (async () => {
     try {
+        await writeFile('file.csv', '');
         const response = await axios.get('https://api.thenewboston.com/core_members');
         const teamMembers = response.data.results;
-        const { data } = await axios.get(`https://api.github.com/repos/thenewboston-developers/Contributor-Payments/issues/${issueId}`);
-        const { labels, html_url, title, body, user } = data;
-        let timesheetApproved = labels.find(label => label.name === '😍+Approved+😍');
-        timesheetApproved = timesheetApproved ? 'Yes' : 'No';
-        let date = title.match(/\d\d\/\d\d\/\d\d\d\d/);
-            date = date ? date[0].replace(/\//g, '_') : false;
-        const contributor = title.split(' - ')[2];
-        const timesheetGithubLink = html_url;
-        let totalTimeSpent = body.match(/total time spent\r\n\.?\d+\.?(0|5)? hour/i) ? body.match(/total time spent\r\n\.?\d+\.?(0|5)? hour/i)[0] : 0;
-        if (!totalTimeSpent) {
-            const timeSpent = body.match(/time spent\r\n\.?\d+\.?(0|5)? hour/i);
-            totalTimeSpent = timeSpent && timeSpent.input ? timeSpent[0] : timeSpent && timeSpent.length > 1 ? timeSpent[timeSpent.length - 1] : 0;
-        }
-        totalTimeSpent = totalTimeSpent ? totalTimeSpent.match(/\d+\.?(0|5)?/)[0] : 0;
-        const accountNumber = body.match(/[a-z0-9]{64}/) ? body.match(/[a-z0-9]{64}/)[0] : false;
-        
-        const memoNote = `TNB_TS_${issueId}_${date}`;
-
-        const member = teamMembers.find(member => {
-            if (member.user.github_username === user.login ||
-                title.toLowerCase().includes(member.user.display_name.toLowerCase()) ||
-                member.user.account_number === accountNumber) {
-                return member;
+        for (const issueId of issueIds) {
+            const { data } = await axios.get(`https://api.github.com/repos/thenewboston-developers/Contributor-Payments/issues/${issueId}`);
+            const { labels, html_url, title, body, user } = data;
+            let timesheetApproved = labels.find(label => label.description === 'Approved' && label.name.includes('Approved'));
+            timesheetApproved = timesheetApproved ? 'Yes' : 'No';
+            const slashDate = title.match(/\d\d\/\d\d\/\d\d\d\d/);
+            const date = slashDate ? slashDate[0].replace(/\//g, '_') : false;
+            const contributor = title.split('-')[2].trim();
+            const timesheetGithubLink = html_url;
+            let totalTimeSpent = body.match(/total time spent\r\n *\.?\d+\.?(0|5)? *hour/i) ? body.match(/total time spent\r\n *\.?\d+\.?(0|5)? *hour/i)[0] : 0;
+            if (!totalTimeSpent) {
+                const timeSpent = body.match(/time spent\r\n *\.?\d+\.?(0|5)? *hour/i);
+                totalTimeSpent = timeSpent && timeSpent.input ? timeSpent[0] : timeSpent && timeSpent.length > 1 ? timeSpent[timeSpent.length - 1] : 0;
             }
-        });
-        const hourlyRate = member && member.hourly_rate ? member.hourly_rate : 0;
-        console.log(`${date}, ${contributor}, ${timesheetApproved}, ${timesheetGithubLink}, ${hourlyRate}, ${totalTimeSpent}, Total Pay, ${accountNumber}, ${memoNote}`);
+            totalTimeSpent = totalTimeSpent ? totalTimeSpent.match(/\.?\d+\.?(0|5)?/)[0] : 0;
+            const accountNumber = body.match(/[a-z0-9]{64}/) ? body.match(/[a-z0-9]{64}/)[0] : false;
+            
+            const memoNote = `TNB_TS_${issueId}_${date}`;
+    
+            const member = teamMembers.find(member => {
+                if (member.user.github_username === user.login ||
+                    title.toLowerCase().includes(member.user.display_name.toLowerCase()) ||
+                    member.user.account_number === accountNumber) {
+                    return member;
+                }
+            });
+            const hourlyRate = member && member.hourly_rate ? member.hourly_rate : 0;
+            const totalPay = hourlyRate * totalTimeSpent;
+            const csvRow = `${slashDate}, ${contributor}, ${timesheetApproved}, ${timesheetGithubLink}, ${hourlyRate}, ${totalTimeSpent}, ${totalPay}, ${accountNumber}, ${memoNote}\n`;
+            // append data to a file
+            await appendFile('file.csv', csvRow);
+        }
     } catch (err) {
         console.log('ERROR', err);
     }
